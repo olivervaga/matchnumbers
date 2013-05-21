ac32c583c5ee45bd263cf88a13b581790a264a8f
Oliver Vaga <oliver.vaga@gmail.com>
2013-05-21 16:48:38 +0300

    Changed highlight color

--- src/eu/j0ntech/tenpair/view/TileDrawer.java
+++ src/eu/j0ntech/tenpair/view/TileDrawer.java
@@ -9,28 +9,25 @@ import eu.j0ntech.tenpair.game.Tile.TileType;
 public class TileDrawer {
 
 	public static final int COLOR_DEFAULT_SQUARE = Color.WHITE;
-	public static final int COLOR_SELECTED_SQUARE = Color.rgb(135, 206, 250); // Light
-																				// blue
-	public static final int COLOR_HIGHLIGHTED_SQUARE = Color.GREEN;
-	public static final int COLOR_SCRATCHED_SQUARE = Color.RED;
+	public static final int COLOR_SELECTED_SQUARE = Color.rgb(135, 206, 250); // Light blue
+	public static final int COLOR_HIGHLIGHTED_SQUARE = Color.rgb(72, 209, 204); // Turquoise
 	public static final int COLOR_NUMBER = Color.BLACK;
 	public static final int COLOR_BACKGROUND = Color.DKGRAY;
 
 	private Paint mRectPaint = new Paint();
 	private Paint mNumberPaint = new Paint();
 	private Paint mLinePaint = new Paint();
-	
+
 	private float tileSize;
 	private int tile3rd;
 	private int tile2_3rds;
 
 	public TileDrawer(BoardView boardView) {
-		
+
 		tileSize = boardView.getTileSize();
 		tile3rd = (int) (tileSize / 3);
 		tile2_3rds = (int) ((tileSize / 3) * 2);
-		
-		
+
 		mRectPaint.setStyle(Paint.Style.FILL);
 		mRectPaint.setAntiAlias(true);
 
@@ -45,8 +42,7 @@ public class TileDrawer {
 		mNumberPaint.setTextAlign(Paint.Align.CENTER);
 	}
 
-	public void draw(Tile tile, Canvas boardCanvas,
-			float offset) {
+	public void draw(Tile tile, Canvas boardCanvas, float offset) {
 		float startX = tile.getStartX() - BoardView.SQUARE_PADDING;
 		float startY = tile.getStartY() - BoardView.SQUARE_PADDING;
 
@@ -68,41 +64,38 @@ public class TileDrawer {
 		}
 		boardCanvas.drawRect(tile.getStartX(), tile.getStartY() + offset,
 				tile.getEndX(), tile.getEndY() + offset, mRectPaint);
-		boardCanvas.drawText(
-				String.valueOf(tile.getValue()),
-				tile.getCenterX(),
-				(float) (tile.getCenterY() + (mNumberPaint.getTextSize()) / 2.6 + offset),
-				mNumberPaint);
+		boardCanvas
+				.drawText(
+						String.valueOf(tile.getValue()),
+						tile.getCenterX(),
+						(float) (tile.getCenterY()
+								+ (mNumberPaint.getTextSize()) / 2.6 + offset),
+						mNumberPaint);
 		if (tile.getType() == TileType.SCRATCHED) {
-			boardCanvas.drawLine(startX + BoardView.SQUARE_PADDING,
-					startY + BoardView.SQUARE_PADDING + offset,
-					startX + tileSize,
+			boardCanvas.drawLine(startX + BoardView.SQUARE_PADDING, startY
+					+ BoardView.SQUARE_PADDING + offset, startX + tileSize,
 					startY + tile3rd + offset, mLinePaint);
-			
-			boardCanvas.drawLine(startX + tileSize,
-					startY + tile3rd + offset,
-					startX + BoardView.SQUARE_PADDING,
-					startY + tile2_3rds + offset, mLinePaint);
-			
-			boardCanvas.drawLine(startX + BoardView.SQUARE_PADDING,
-					startY + tile2_3rds + offset,
-					startX + tileSize,
-					startY + tileSize + offset, mLinePaint);
-			
-			boardCanvas.drawLine(startX + tileSize,
-					startY + BoardView.SQUARE_PADDING + offset,
-					startX + BoardView.SQUARE_PADDING,
-					startY + tile3rd + offset, mLinePaint);
-			
-			boardCanvas.drawLine(startX + BoardView.SQUARE_PADDING,
-					startY + tile3rd + offset,
-					startX + tileSize,
-					startY + tile2_3rds + offset, mLinePaint);
-			
-			boardCanvas.drawLine(startX + tileSize,
-					startY + tile2_3rds + offset,
-					startX + BoardView.SQUARE_PADDING,
-					startY + tileSize + offset, mLinePaint);
+
+			boardCanvas.drawLine(startX + tileSize, startY + tile3rd + offset,
+					startX + BoardView.SQUARE_PADDING, startY + tile2_3rds
+							+ offset, mLinePaint);
+
+			boardCanvas.drawLine(startX + BoardView.SQUARE_PADDING, startY
+					+ tile2_3rds + offset, startX + tileSize, startY + tileSize
+					+ offset, mLinePaint);
+
+			boardCanvas.drawLine(startX + tileSize, startY
+					+ BoardView.SQUARE_PADDING + offset, startX
+					+ BoardView.SQUARE_PADDING, startY + tile3rd + offset,
+					mLinePaint);
+
+			boardCanvas.drawLine(startX + BoardView.SQUARE_PADDING, startY
+					+ tile3rd + offset, startX + tileSize, startY + tile2_3rds
+					+ offset, mLinePaint);
+
+			boardCanvas.drawLine(startX + tileSize, startY + tile2_3rds
+					+ offset, startX + BoardView.SQUARE_PADDING, startY
+					+ tileSize + offset, mLinePaint);
 		}
 	}
 
